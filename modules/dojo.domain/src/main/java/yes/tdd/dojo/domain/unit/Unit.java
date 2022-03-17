29f77257462d16b1078fbab8f71258f61e7e7fb4
Neil Wang <wangweili457@gmail.com>
2022-03-17 16:51:33 +0800

    DONE: should_be_able_to_transfer_13_5_dm_to_135_cm

--- modules/dojo.domain/src/main/java/yes/tdd/dojo/domain/unit/Unit.java
+++ modules/dojo.domain/src/main/java/yes/tdd/dojo/domain/unit/Unit.java
@@ -8,15 +8,15 @@ public class Unit {
     private static final int CM = 100;
     private static final int DM = 10;
     private static final int M = 1;
-    private static final Map<String, Integer> UNIT_MAP = Map.of(
+    private static final Map<String, Integer> FORMULA_MAP = Map.of(
         "cm", CM, "dm", DM, "m", M
     );
 
     public Unit(Double value, String unit) {
-        this.value = value / UNIT_MAP.get(unit);
+        this.value = value / FORMULA_MAP.get(unit);
     }
 
     public Double to(String unit) {
-        return this.value * UNIT_MAP.get(unit);
+        return this.value * FORMULA_MAP.get(unit);
     }
 }
--- modules/dojo.domain/src/test/java/yes/tdd/dojo/domain/unit/UnitTest.java
+++ modules/dojo.domain/src/test/java/yes/tdd/dojo/domain/unit/UnitTest.java
@@ -1,32 +1,44 @@
 package yes.tdd.dojo.domain.unit;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UnitTest {
-    @Test
-    void should_be_able_to_transfer_1_m_to_100_cm() {
-        assertEquals(new Unit(1.0, "m").to("cm"), 100.0);
+
+    @Nested
+    class IntegrationTest {
+        @Test
+        void should_be_able_to_transfer_13_5_dm_to_135_cm() {
+            assertEquals(new Unit(13.5, "dm").to("cm"), 135.0);
+        }
     }
 
-    @Test
-    void should_be_able_to_transfer_1_m_to_10_dm() {
-        assertEquals(new Unit(1.0, "m").to("dm"), 10.0);
-    }
+    @Nested
+    class UnitInUnitTest {
+        @Test
+        void should_be_able_to_transfer_1_m_to_100_cm() {
+            assertEquals(new Unit(1.0, "m").to("cm"), 100.0);
+        }
 
-    @Test
-    void should_be_able_to_transfer_1_m_to_1_m() {
-        assertEquals(new Unit(1.0, "m").to("m"), 1.0);
-    }
+        @Test
+        void should_be_able_to_transfer_1_m_to_10_dm() {
+            assertEquals(new Unit(1.0, "m").to("dm"), 10.0);
+        }
 
-    @Test
-    void should_be_able_to_transfer_100cm_to_1_m() {
-        assertEquals(new Unit(100.0, "cm").to("m"), 1.0);
-    }
+        @Test
+        void should_be_able_to_transfer_1_m_to_1_m() {
+            assertEquals(new Unit(1.0, "m").to("m"), 1.0);
+        }
 
-    @Test
-    void should_be_able_to_transfer_10dm_to_1_m() {
-        assertEquals(new Unit(10.0, "dm").to("m"), 1.0);
+        @Test
+        void should_be_able_to_transfer_100cm_to_1_m() {
+            assertEquals(new Unit(100.0, "cm").to("m"), 1.0);
+        }
+
+        @Test
+        void should_be_able_to_transfer_10dm_to_1_m() {
+            assertEquals(new Unit(10.0, "dm").to("m"), 1.0);
+        }
     }
 }
