3f9954bc3bc3b024bfc8e74127ec05a12946edd7
Neil Wang <wangweili457@gmail.com>
2022-03-17 16:44:05 +0800

    DONE: should_be_able_to_transfer_1_m_to_1_m

--- modules/dojo.domain/src/main/java/yes/tdd/dojo/domain/unit/Unit.java
+++ modules/dojo.domain/src/main/java/yes/tdd/dojo/domain/unit/Unit.java
@@ -1,19 +1,22 @@
 package yes.tdd.dojo.domain.unit;
 
+import java.util.Map;
+
 public class Unit {
     private final Double value;
 
     private static final int CM = 100;
     private static final int DM = 10;
     private static final int M = 1;
+    private static final Map<String, Integer> UNIT_MAP = Map.of(
+        "cm", CM, "dm", DM, "m", M
+    );
 
     public Unit(Double value, String unit) {
         this.value = value;
     }
 
     public Double to(String unit) {
-        if (unit.equals("cm")) return value * CM;
-        if (unit.equals("dm")) return value * DM;
-        return value * M;
+        return this.value * UNIT_MAP.get(unit);
     }
 }
