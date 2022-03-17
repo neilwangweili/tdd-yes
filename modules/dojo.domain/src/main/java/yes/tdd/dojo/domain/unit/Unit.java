e8113f37b139aa111bf1cf32908fb0aed958d154
Neil Wang <wangweili457@gmail.com>
2022-03-17 17:09:45 +0800

    REFACTORED: UnitHub

--- modules/dojo.domain/src/main/java/yes/tdd/dojo/domain/unit/Unit.java
+++ modules/dojo.domain/src/main/java/yes/tdd/dojo/domain/unit/Unit.java
@@ -1,16 +1,7 @@
 package yes.tdd.dojo.domain.unit;
 
-import java.util.Map;
-
 public class Unit {
     private final Double value;
-
-    private static final int CM = 100;
-    private static final int DM = 10;
-    private static final int M = 1;
-    private static final Map<String, Integer> FORMULA_MAP = Map.of(
-        "cm", CM, "dm", DM, "m", M
-    );
 
     public Unit(Double value, UnitHub unit) {
         this.value = value / unit.formula();
