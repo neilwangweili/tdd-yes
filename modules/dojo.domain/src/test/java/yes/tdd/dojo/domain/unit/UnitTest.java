18257ace5750d581f4cc04c96c668dfb814fbcfa
Neil Wang <wangweili457@gmail.com>
2022-03-17 16:47:07 +0800

    DONE: should_be_able_to_transfer_10dm_to_1_m

--- modules/dojo.domain/src/test/java/yes/tdd/dojo/domain/unit/UnitTest.java
+++ modules/dojo.domain/src/test/java/yes/tdd/dojo/domain/unit/UnitTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UnitTest {
-    //100cm = 10dm = 1m
     @Test
     void should_be_able_to_transfer_1_m_to_100_cm() {
         assertEquals(new Unit(1.0, "m").to("cm"), 100.0);
@@ -24,5 +23,10 @@ public class UnitTest {
     @Test
     void should_be_able_to_transfer_100cm_to_1_m() {
         assertEquals(new Unit(100.0, "cm").to("m"), 1.0);
+    }
+
+    @Test
+    void should_be_able_to_transfer_10dm_to_1_m() {
+        assertEquals(new Unit(10.0, "dm").to("m"), 1.0);
     }
 }
