238a74c9caccedf0f316add5c400cd1d6c4a0653
Neil Wang <wangweili457@gmail.com>
2022-03-18 22:04:23 +0800

    REFACTORED: GildRoseTest

--- modules/dojo.domain/src/test/java/yes/tdd/dojo/domain/gildedrose/GildedRoseTest.java
+++ modules/dojo.domain/src/test/java/yes/tdd/dojo/domain/gildedrose/GildedRoseTest.java
@@ -1,6 +1,8 @@
 package yes.tdd.dojo.domain.gildedrose;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.io.*;
 import java.util.List;
@@ -60,10 +62,10 @@ public class GildedRoseTest {
 
         private GildedRose gildedRose() {
             List<Item> items = List.of(
-                newInstance("+5 Dexterity Vest", 10, 20), //
-                newInstance("Aged Brie", 2, 0), //
-                newInstance("Elixir of the Mongoose", 5, 7), //
-                newInstance("Sulfuras, Hand of Ragnaros", 0, 80), //
+                newInstance("+5 Dexterity Vest", 10, 20),
+                newInstance("Aged Brie", 2, 0),
+                newInstance("Elixir of the Mongoose", 5, 7),
+                newInstance("Sulfuras, Hand of Ragnaros", 0, 80),
                 newInstance("Sulfuras, Hand of Ragnaros", -1, 80),
                 newInstance("Backstage passes to a TAFKAL80ETC concert", 15, 20),
                 newInstance("Backstage passes to a TAFKAL80ETC concert", 10, 49),
@@ -75,60 +77,23 @@ public class GildedRoseTest {
 
     @Nested
     class UnitTest {
-        @Test
-        void should_be_able_to_reduce_1_quality_of_unexpired_common_item_when_pass_one_day() {
-            test("common item", 10, 10, "common item, 9, 9");
-        }
-
-        @Test
-        void should_be_able_to_reduce_2_quality_of_expired_common_item_when_pass_one_day() {
-            test("common item", 0, 10, "common item, -1, 8");
-        }
-
-        @Test
-        void should_be_able_to_keep_quality_to_positive_if_common_item_quality_reduce_to_negative() {
-            test("common item", 0, 0, "common item, -1, 0");
-        }
-
-        @Test
-        void should_be_able_to_add_1_quality_if_item_is_aged_brie() {
-            test("Aged Brie", 10, 0, "Aged Brie, 9, 1");
-        }
-
-        @Test
-        void should_be_able_to_keep_quality_to_50_if_aged_brie_quality_increase_up_on_50() {
-            test("Aged Brie", 10, 50, "Aged Brie, 9, 50");
-        }
-
-        @Test
-        void should_be_able_to_keep_quality_and_sell_in_when_item_is_sulfuras() {
-            test("Sulfuras, Hand of Ragnaros", 0, 80, "Sulfuras, Hand of Ragnaros, 0, 80");
-        }
-
-        @Test
-        void should_be_able_to_add_1_quality_of_backstage_pass_when_pass_one_day() {
-            test("Backstage passes to a TAFKAL80ETC concert", 50, 10, "Backstage passes to a TAFKAL80ETC concert, 49, 11");
-        }
-
-        @Test
-        void should_be_able_to_add_2_quality_of_backstage_pass_when_sell_in_is_in_10() {
-            test("Backstage passes to a TAFKAL80ETC concert", 10, 10, "Backstage passes to a TAFKAL80ETC concert, 9, 12");
-        }
-
-        @Test
-        void should_be_able_to_add_3_quality_of_backstage_pass_when_sell_in_is_in_5() {
-            test("Backstage passes to a TAFKAL80ETC concert", 5, 10, "Backstage passes to a TAFKAL80ETC concert, 4, 13");
-        }
-
-        @Test
-        void should_be_able_to_have_none_quality_of_backstage_when_sell_in_is_0() {
-            test("Backstage passes to a TAFKAL80ETC concert", 0, 10, "Backstage passes to a TAFKAL80ETC concert, -1, 0");
-        }
-
-        private void test(String item, int sellIn, int quality, String actual) {
-            Item commonItem = newInstance(newInstance(item, sellIn, quality));
-            commonItem.updateQuality();
-            assertEquals(commonItem.toString(), actual);
+        @ParameterizedTest
+        @CsvSource({
+            "'common item',                                 10,     10,         'common item, 9, 9'",
+            "'common item',                                 0,      10,         'common item, -1, 8'",
+            "'common item',                                 0,      0,          'common item, -1, 0'",
+            "'Aged Brie',                                   10,     0,          'Aged Brie, 9, 1'",
+            "'Aged Brie',                                   10,     50,         'Aged Brie, 9, 50'",
+            "'Sulfuras, Hand of Ragnaros',                  0,      80,         'Sulfuras, Hand of Ragnaros, 0, 80'",
+            "'Backstage passes to a TAFKAL80ETC concert',   50,     10,         'Backstage passes to a TAFKAL80ETC concert, 49, 11'",
+            "'Backstage passes to a TAFKAL80ETC concert',   10,     10,         'Backstage passes to a TAFKAL80ETC concert, 9, 12'",
+            "'Backstage passes to a TAFKAL80ETC concert',   5,      10,         'Backstage passes to a TAFKAL80ETC concert, 4, 13'",
+            "'Backstage passes to a TAFKAL80ETC concert',   0,      10,         'Backstage passes to a TAFKAL80ETC concert, -1, 0'"
+        })
+        void should_be_able_to_change_quality_of_one_item_when_pass_one_day(String name, int sellIn, int quality, String actual) {
+            Item item = newInstance(newInstance(name, sellIn, quality));
+            item.updateQuality();
+            assertEquals(item.toString(), actual);
         }
     }
 }
