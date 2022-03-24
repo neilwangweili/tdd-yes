c604dec1584eef20e8eefeb4f6a726bdd5d865c0
Neil Wang <wangweili457@gmail.com>
2022-03-24 12:23:06 +0800

    REFACTORED: Args.

--- modules/dojo.domain/src/main/java/yes/tdd/dojo/domain/args/Args.java
+++ modules/dojo.domain/src/main/java/yes/tdd/dojo/domain/args/Args.java
@@ -7,21 +7,7 @@ import java.util.*;
 
 import static yes.tdd.dojo.domain.args.OptionParsers.*;
 
-public final class Args {
-    private Args() {
-    }
-
-    @SuppressWarnings("all")
-    public static <T> T parse(Class<T> optionClass, String... args) {
-        Constructor<?> constructor = optionClass.getDeclaredConstructors()[0];
-        Object[] values = Arrays.stream(constructor.getParameters()).map(o -> parseOption(o, Arrays.asList(args))).toArray();
-        return Exceptions.evaluate(() -> (T) constructor.newInstance(values));
-    }
-
-    private static Object parseOption(Parameter parameter, List<String> arguments) {
-        return getOptionalParser(parameter).parse(arguments, parameter.getAnnotation(Option.class));
-    }
-
+public final class Args<T> {
     private static final Map<Class<?>, OptionParser<?>> PARSERS = Map.of(
         boolean.class, bool(),
         int.class, unary(0, Integer::parseInt),
@@ -29,7 +15,31 @@ public final class Args {
         String[].class, list(String[]::new, String::valueOf),
         Integer[].class, list(Integer[]::new, Integer::parseInt));
 
-    private static OptionParser<?> getOptionalParser(Parameter parameter) {
-        return PARSERS.get(parameter.getType());
+    private final Class<T> optionClass;
+    private final Map<Class<?>, OptionParser<?>> parsers;
+
+
+    public Args(Class<T> optionClass, Map<Class<?>, OptionParser<?>> parsers) {
+        this.optionClass = optionClass;
+        this.parsers = parsers;
+    }
+
+    @SuppressWarnings("all")
+    public T parse(String... args) {
+        Constructor<?> constructor = optionClass.getDeclaredConstructors()[0];
+        Object[] values = Arrays.stream(constructor.getParameters()).map(o -> parseOption(o, Arrays.asList(args))).toArray();
+        return Exceptions.evaluate(() -> (T) constructor.newInstance(values));
+    }
+
+    public static <T> T parse(Class<T> optionClass, String... args) {
+        return new Args<>(optionClass, PARSERS).parse(args);
+    }
+
+    private Object parseOption(Parameter parameter, List<String> arguments) {
+        return getOptionalParser(parameter).parse(arguments, parameter.getAnnotation(Option.class));
+    }
+
+    private OptionParser<?> getOptionalParser(Parameter parameter) {
+        return parsers.get(parameter.getType());
     }
 }
--- modules/dojo.domain/src/test/java/yes/tdd/dojo/domain/args/ArgsTest.java
+++ modules/dojo.domain/src/test/java/yes/tdd/dojo/domain/args/ArgsTest.java
@@ -1,22 +1,47 @@
 package yes.tdd.dojo.domain.args;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 public class ArgsTest {
-    @Test
-    void should_be_able_to_parse_bool_int_directory_by_args() {
-        MultipleOptions options = Args.parse(MultipleOptions.class, "-l", "-p", "8080", "-d", "/usr/logs");
-        assertTrue(options.logging());
-        assertEquals(options.port(), 8080);
-        assertEquals(options.directory(), "/usr/logs");
+    @Nested
+    class IntegrationTest {
+        @Test
+        void should_be_able_to_parse_bool_int_directory_by_args() {
+            MultipleOptions options = Args.parse(MultipleOptions.class, "-l", "-p", "8080", "-d", "/usr/logs");
+            assertTrue(options.logging());
+            assertEquals(options.port(), 8080);
+            assertEquals(options.directory(), "/usr/logs");
+        }
+
+        @Test
+        void should_be_able_to_parse_list_by_args() {
+            ListOptions options = Args.parse(ListOptions.class, "-g", "this", "is", "a", "list", "-d", "1", "2", "-3", "5");
+            assertArrayEquals(new String[]{"this", "is", "a", "list"}, options.group());
+            assertArrayEquals(new Integer[]{1, 2, -3, 5}, options.decimals());
+        }
+
     }
 
-    @Test
-    void should_be_able_to_parse_list_by_args() {
-        ListOptions options = Args.parse(ListOptions.class, "-g", "this", "is", "a", "list", "-d", "1", "2", "-3", "5");
-        assertArrayEquals(new String[]{"this", "is", "a", "list"}, options.group());
-        assertArrayEquals(new Integer[]{1, 2, -3, 5}, options.decimals());
+    @Nested
+    class UnitTest {
+        @Test
+        void should_parse_option_parsers_if_option_parser_provided() {
+            OptionParser<Boolean> boolParser = mock(OptionParser.class);
+            OptionParser<Integer> intParser = mock(OptionParser.class);
+            OptionParser<String> stringParser = mock(OptionParser.class);
+            when(boolParser.parse(any(), any())).thenReturn(true);
+            when(intParser.parse(any(), any())).thenReturn(1);
+            when(stringParser.parse(any(), any())).thenReturn("Hello");
+            Args<MultipleOptions> args = new Args<>(MultipleOptions.class, Map.of(boolean.class, boolParser, int.class, intParser, String.class, stringParser));
+            MultipleOptions options = args.parse("-l", "-p", "8080", "-d", "/usr/logs");
+            assertTrue(options.logging());
+            assertEquals(options.port(), 1);
+            assertEquals(options.directory(), "Hello");
+        }
     }
 }
