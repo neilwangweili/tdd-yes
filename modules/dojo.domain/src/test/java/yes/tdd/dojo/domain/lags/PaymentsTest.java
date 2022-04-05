ff476f637ce3ef1b5102a64dbde50be6f2626331
Neil Wang <wangweili457@gmail.com>
2022-04-05 20:01:57 +0800

    DONE: PaymentsTest.

--- modules/dojo.domain/src/test/java/yes/tdd/dojo/domain/lags/PaymentsTest.java
+++ modules/dojo.domain/src/test/java/yes/tdd/dojo/domain/lags/PaymentsTest.java
@@ -1,18 +1,21 @@
 package yes.tdd.dojo.domain.lags;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PaymentsTest {
-    @Test
-    void should_be_able_to_calculate_the_best_advice_giving_by_orders() {
-        Payments payments = Payments.create(
-            Order.of("AF514", 0, 5, 10),
-            Order.of("C05", 3, 7, 14),
-            Order.of("AF515", 5, 9, 7),
-            Order.of("BA01", 6, 9, 8)
-        );
-        assertEquals(payments.findBest(), "The best choices is: AF514 BA01. The gain is 18");
+    @Nested
+    class IntegrationTest {
+        @Test
+        void should_be_able_to_calculate_the_best_advice_giving_by_orders() {
+            Payments payments = Payments.create(
+                Order.of("AF514", 0, 5, 10),
+                Order.of("C05", 3, 7, 14),
+                Order.of("AF515", 5, 9, 7),
+                Order.of("BA01", 6, 9, 8)
+            );
+            assertEquals(payments.findBest(), "The best choices is: AF514 BA01. The gain is 18");
+        }
     }
 }
