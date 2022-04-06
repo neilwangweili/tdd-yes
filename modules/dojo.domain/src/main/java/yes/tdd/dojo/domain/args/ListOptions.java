2bc9f6230b61b342bd50d75ec9af41bd7e2c6260
Neil Wang <wangweili457@gmail.com>
2022-04-06 10:41:18 +0800

    REFACTORED: To record.

--- modules/dojo.domain/src/main/java/yes/tdd/dojo/domain/args/ListOptions.java
+++ modules/dojo.domain/src/main/java/yes/tdd/dojo/domain/args/ListOptions.java
@@ -1,19 +1,4 @@
 package yes.tdd.dojo.domain.args;
 
-public class ListOptions {
-    private final String[] group;
-    private final Integer[] decimals;
-
-    public ListOptions(@Option("-g") String[] group, @Option("-d") Integer[] decimals) {
-        this.group = group;
-        this.decimals = decimals;
-    }
-
-    public String[] group() {
-        return group;
-    }
-
-    public Integer[] decimals() {
-        return decimals;
-    }
+public record ListOptions(@Option("-g") String[] group, @Option("-d") Integer[] decimals) {
 }
--- modules/dojo.domain/src/main/java/yes/tdd/dojo/domain/args/MultipleOptions.java
+++ modules/dojo.domain/src/main/java/yes/tdd/dojo/domain/args/MultipleOptions.java
@@ -1,25 +1,4 @@
 package yes.tdd.dojo.domain.args;
 
-public class MultipleOptions {
-    private final boolean logging;
-    private final int port;
-    private final String directory;
-
-    public MultipleOptions(@Option("-l") boolean logging, @Option("-p") int port, @Option("-d") String directory) {
-        this.logging = logging;
-        this.port = port;
-        this.directory = directory;
-    }
-
-    public boolean logging() {
-        return logging;
-    }
-
-    public int port() {
-        return port;
-    }
-
-    public String directory() {
-        return directory;
-    }
+public record MultipleOptions(@Option("-l") boolean logging, @Option("-p") int port, @Option("-d") String directory) {
 }
