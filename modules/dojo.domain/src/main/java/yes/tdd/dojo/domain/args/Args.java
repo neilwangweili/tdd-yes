b7866608481b3dee78d7b031d21e60ce0f60149a
Neil Wang <wangweili457@gmail.com>
2022-05-14 03:30:43 +0800

    REFACTORED: Args.

--- modules/dojo.domain/src/main/java/yes/tdd/dojo/domain/args/Args.java
+++ modules/dojo.domain/src/main/java/yes/tdd/dojo/domain/args/Args.java
@@ -1,10 +1,13 @@
 package yes.tdd.dojo.domain.args;
 
-import java.util.*;
+import java.util.Map;
 
 import static yes.tdd.dojo.domain.args.OptionParsers.*;
 
-public final class Args<T> {
+public final class Args {
+    private Args() {
+    }
+
     private static final Map<Class<?>, OptionParser<?>> PARSERS = Map.of(
         boolean.class, bool(),
         int.class, unary(0, Integer::parseInt),
@@ -12,21 +15,7 @@ public final class Args<T> {
         String[].class, list(String::valueOf, String[]::new),
         Integer[].class, list(Integer::parseInt, Integer[]::new));
 
-    private final Class<T> optionClass;
-    private final Map<Class<?>, OptionParser<?>> parsers;
-
-
-    public Args(Class<T> optionClass, Map<Class<?>, OptionParser<?>> parsers) {
-        this.optionClass = optionClass;
-        this.parsers = parsers;
-    }
-
-
-    public T parse(String... args) {
-        return new OptionsClass<>(optionClass).parse(args, this.parsers);
-    }
-
     public static <T> T parse(Class<T> optionClass, String... args) {
-        return new Args<>(optionClass, PARSERS).parse(args);
+        return new OptionsClass<>(PARSERS, optionClass).parse(args);
     }
 }
--- modules/dojo.domain/src/main/java/yes/tdd/dojo/domain/args/OptionsClass.java
+++ modules/dojo.domain/src/main/java/yes/tdd/dojo/domain/args/OptionsClass.java
@@ -6,24 +6,26 @@ import java.lang.reflect.*;
 import java.util.*;
 
 public class OptionsClass<T> {
+    private final Map<Class<?>, OptionParser<?>> parsers;
     private final Class<T> optionsClass;
 
-    public OptionsClass(Class<T> optionsClass) {
+    public OptionsClass(Map<Class<?>, OptionParser<?>> parsers, Class<T> optionsClass) {
+        this.parsers = parsers;
         this.optionsClass = optionsClass;
     }
 
     @SuppressWarnings("all")
-    public T parse(String[] args, Map<Class<?>, OptionParser<?>> parsers) {
+    public <T> T parse(String[] args) {
         Constructor<?> constructor = this.optionsClass.getDeclaredConstructors()[0];
-        Object[] values = Arrays.stream(constructor.getParameters()).map(o -> parseOption(parsers, o, Arrays.asList(args))).toArray();
+        Object[] values = Arrays.stream(constructor.getParameters()).map(o -> this.parseOption(o, Arrays.asList(args))).toArray();
         return Exceptions.evaluate(() -> (T) constructor.newInstance(values));
     }
 
-    private Object parseOption(Map<Class<?>, OptionParser<?>> parsers, Parameter parameter, List<String> arguments) {
-        return getOptionalParser(parsers, parameter).parse(arguments, parameter.getAnnotation(Option.class));
+    private Object parseOption(Parameter parameter, List<String> arguments) {
+        return getOptionalParser(parameter).parse(arguments, parameter.getAnnotation(Option.class));
     }
 
-    private OptionParser<?> getOptionalParser(Map<Class<?>, OptionParser<?>> parsers, Parameter parameter) {
-        return parsers.get(parameter.getType());
+    private OptionParser<?> getOptionalParser(Parameter parameter) {
+        return this.parsers.get(parameter.getType());
     }
 }
